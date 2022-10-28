c205bee6eb71c25e155368b5c0986f26ba14b6e1
gamingamerman <gamingamerman>
2022-10-28 17:26:53 +0200

    commit 4

--- src/Player.js
+++ src/Player.js
@@ -2,7 +2,7 @@ class Player {
     #gender;
     #age;
 
-    constructor(name, gender, date_of_birth, category) {
+    constructor(name, gender, date_of_birth) {
         this.name = name;
         if (gender !== 'male' && gender !=='female') {
             this.#gender = 'female';
@@ -13,7 +13,6 @@ class Player {
             const [d,m,y] = date_of_birth.split('/');
             this.date_of_birth = new Date(y,m,d);
         }
-        this.category = category;
     }
 
     get gender() {
@@ -26,10 +25,26 @@ class Player {
         return age
     }
 
-    category() {
+    get category() {
         let today = new Date();
         let age = today.getFullYear() - this.date_of_birth.getFullYear()
-        
+        switch (age) {
+            case (age < 12):
+                return "Benjamin"
+                break;
+            case (age > 12 && age < 15):
+                return "Cadet" 
+                break;
+            case (age > 16 && age < 19):
+                return "Junior"
+                break;
+            case (age > 20):
+                return "Senior"
+                break; 
+            default:
+                return "Error"
+                break;
+        }
     }
 }
 
